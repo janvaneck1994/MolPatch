d5d9feaca58003645d49fd68f0ec61d14cb26613
janvaneck1994 <janvaneck94@gmail.com>
2024-11-07 16:29:35 +0100

    finish visuals

--- chimera.py
+++ chimera.py
@@ -4,18 +4,23 @@ import sys
 
 def parse_csv(csv_file):
     patch_rank_0 = []
+    low_plddt = []
     all_residues = []
     with open(csv_file, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             residue_spec = "{}.{}".format(row['residue_id'], row['chain'])
             all_residues.append(residue_spec)
-            if row['patch_rank'] == '0':
+            print(row['plddt'])
+            if row['patch_rank'] == '0' and row['plddt'] and float(row['plddt']) < 70:
+                low_plddt.append(residue_spec)
+            elif row['patch_rank'] == '0':
                 patch_rank_0.append(residue_spec)
-    return patch_rank_0, all_residues
+
+    return patch_rank_0, low_plddt, all_residues
 
 def color_residues(pdb_file, csv_file):
-    patch_rank_0, all_residues = parse_csv(csv_file)
+    patch_rank_0, low_plddt, all_residues = parse_csv(csv_file)
     print(all_residues)
     runCommand('open {}'.format(pdb_file))
     runCommand('color blue')
@@ -25,6 +30,9 @@ def color_residues(pdb_file, csv_file):
     
     for residue in patch_rank_0:
         runCommand('color red :{}'.format(residue))
+
+    for residue in low_plddt:
+        runCommand('color yellow :{}'.format(residue))
     
     runCommand('surface')
     runCommand('~ribbon')
